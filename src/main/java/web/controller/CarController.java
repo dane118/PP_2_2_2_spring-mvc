5826b4007de32296f659e9de51db38c4157e8f4f
Dani <120296813+dane118@users.noreply.github.com>
2023-11-30 00:29:32 +0700

    Work on comments
    добавил аннотации @Repository, @Service. Добавил defaultValue в @RequestParam. Внедрил зависимости в CarService

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,19 +1,20 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-import web.service.CarServiceImpl;
 
 @Controller
 public class CarController {
-    private CarService service = new CarServiceImpl();
+    @Autowired
+    private CarService service;
     @GetMapping(value = "/cars")
-    public String printCar(@RequestParam(value = "count", required = false) Integer count, Model model) {
+    public String printCar(@RequestParam(value = "count", required = false, defaultValue = "5") Integer count, Model model) {
 
-        model.addAttribute("cars", service.listCarWithCount((count == null) ? 5 : count));
+        model.addAttribute("cars", service.listCarWithCount(count));
 
         return "cars";
     }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,11 +1,13 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.dao.CarDao;
 import web.dao.CarDaoImpl;
 import web.model.Car;
 
 import java.util.List;
 
+@Service
 public class CarServiceImpl implements CarService{
 
     private CarDao carDao = new CarDaoImpl();
