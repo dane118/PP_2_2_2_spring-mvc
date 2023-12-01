ac21f7a029fb1e785fb8c2f0e0545c9f10491132
Dani <120296813+dane118@users.noreply.github.com>
2023-12-01 10:33:43 +0700

    Complete Task1
    Поправил Autowiring в Service

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,16 +1,18 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.dao.CarDao;
-import web.dao.CarDaoImpl;
 import web.model.Car;
 
 import java.util.List;
 
 @Service
-public class CarServiceImpl implements CarService{
+public class CarServiceImpl implements CarService {
 
-    private CarDao carDao = new CarDaoImpl();
+    @Autowired
+    private CarDao carDao;
+
     @Override
     public List<Car> listCarWithCount(Integer count) {
         return carDao.listCarWithCount(count);
